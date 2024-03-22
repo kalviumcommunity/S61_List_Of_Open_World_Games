2a1de516a1eeb301184649ebe85e826a11c43a82
BOMMAKAPARANDHAMAREDDY <149357107+BOMMAKAPARANDHAMAREDDY@users.noreply.github.com>
2024-03-22 09:58:16 +0530

    connected frontend and backend

--- Frontend/src/components/Entity.jsx
+++ Frontend/src/components/Entity.jsx
@@ -1,42 +1,51 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
-import data from "../../data.json";
+import React, { useEffect, useState } from "react";
+// import data from "../../data.json";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import axios from "axios";
 
 const Entity = () => {
+const [data,setData] = useState(null)
+
   useEffect(() => {
-    fetch("http://localhost:8000/api/data")
+    fetch("https://s61-list-of-open-world-games.onrender.com/api/data")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setData(data.data));
   }, []);
+  console.log(data)
 
   return (
-    <div className="mx-auto m-[40px] max-w-lg p-[20px] shadow-md bg-gray-200 text-white rounded-lg">
+    <div>
+      {
+        data && data.map((game, i) => (
+          <div className="mx-auto m-[40px] max-w-lg p-[20px] shadow-md bg-gray-200 text-white rounded-lg" key={i}>
       <Card className="text-center">
         <CardContent>
-          <div className="text-4xl font-bold mb-4">{data.id}</div>
+          <div className="text-4xl font-bold mb-4">{game.id}</div>
           <div className="text-xl p-[10px]">
-            <strong>Game Title</strong> : {data.gameTitle}
+            <strong>Game Title</strong> : {game.gameTitle}
           </div>
           <div className="text-lg p-[10px]">
-            <strong>Published By</strong> : {data.publishedBy}
+            <strong>Published By</strong> : {game.publishedBy}
           </div>
           <div className="text-lg p-[10px]">
-            <strong>Year Of Release</strong> : {data.yearOfRelease}
+            <strong>Year Of Release</strong> : {game.yearOfRelease}
           </div>
           <div className="text-lg p-[10px]">
-            <strong>Available Platforms</strong> : {data.availablePlatforms}
+            <strong>Available Platforms</strong> : {game.availablePlatforms}
           </div>
           <div className="text-lg p-[10px]">
-            <strong>Genre</strong> : {data.genre}
+            <strong>Genre</strong> : {game.genre}
           </div>
           <div className="text-lg p-[10px]">
-            <strong>Description</strong> : {data.description}
+            <strong>Description</strong> : {game.description}
           </div>
         </CardContent>
       </Card>
+    </div>
+        ))
+      }
     </div>
   );
 };
